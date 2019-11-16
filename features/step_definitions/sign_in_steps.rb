24fe9423e593ddfb4fbcc11b4cbd606fa00205a1
artgolovin <halavin.artsiom@gmail.com>
2019-11-16 12:43:14 +0300

    Added syntax sugar for Page Object - SignInForm

--- features/step_definitions/sign_in_steps.rb
+++ features/step_definitions/sign_in_steps.rb
@@ -1,9 +1,13 @@
+# I have to manually destroy user session, because:
+# - Default Capybara webdriver RackTest doesn't clear after each scenario
+# - Selenium Driver has conflict with testing Devise Auth(RSpec can be configured to deal with it)
+# But not the Cucumber-Rails
 Given('There is an administrator') do
-  @admin = FactoryBot.create(:administrator)
+  @admin = FactoryBot.create :administrator
 end
 
 When('I sign as an administrator with correct credentials') do
-  SignInForm.new.visit_page.fill_in_with(
+  SignInForm.fill_in_with(
     email: @admin.email,
     password: @admin.password
   ).submit
@@ -15,15 +19,15 @@ end
 
 Then('Page must contain welcome message for administrator') do
   expect(page).to have_content("Welcome #{@admin.role.name}, #{@admin.first_name}")
-  click_link_or_button("Sign out")
+  visit destroy_user_session_url
 end
 
 Given('There is a user') do
-  @user = FactoryBot.create(:user)
+  @user = FactoryBot.create :user
 end
 
 When('I am sign as an user with correct credentials') do
-  SignInForm.new.visit_page.fill_in_with(
+  SignInForm.fill_in_with(
     email: @user.email,
     password: @user.password
   ).submit
@@ -31,20 +35,20 @@ end
 
 Then('Page must contain welcome message for user') do
   expect(page).to have_content("Welcome #{@user.role.name}, #{@user.first_name}")
-  click_link_or_button("Sign out")
+  visit destroy_user_session_url
 end
 
 Given('There is no person with this credentials') do
-  @anonym = FactoryBot.attributes_for(:user)
+  @anonym = FactoryBot.attributes_for :user
 end
 
 When('I try to sign in with this credentials') do
-  SignInForm.new.visit_page.fill_in_with(
+  SignInForm.fill_in_with(
     email: @anonym[:email],
     password: @anonym[:password]
   ).submit
 end
 
 Then('Page must contain warning about wrong credentials') do
-  expect(page).to have_content("Invalid Email or password")
+  expect(page).to have_content('Invalid Email or password')
 end
--- features/support/sign_in_form.rb
+++ features/support/sign_in_form.rb
@@ -1,20 +1,28 @@
-class SignInForm
-  include Capybara::DSL
+module SignInForm
+  extend Capybara::DSL
 
-  def visit_page
-    visit('/')
-    click_link_or_button('Sign in')
-    self
-  end
+  class << self
+    def fill_in_with(creds)
+      visit_sign_in_page
+      fill_form creds
+    end
 
-  def fill_in_with(creds = {})
-    fill_in 'Email', with: creds[:email]
-    fill_in 'Password', with: creds[:password]
-    self
-  end
+    def submit
+      click_link_or_button 'Submit'
+    end
 
-  def submit
-    click_link_or_button 'Submit'
-    self
+    private
+
+    def visit_sign_in_page
+      visit :root_url
+      click_link_or_button 'Sign in'
+      self
+    end
+
+    def fill_form(creds)
+      fill_in 'Email', with: creds[:email]
+      fill_in 'Password', with: creds[:password]
+      self
+    end
   end
 end
