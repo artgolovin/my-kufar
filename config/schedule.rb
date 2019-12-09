3b7b0eb7ffdc839c0b47082e9ef934f8f6eef776
artgolovin <halavin.artsiom@gmail.com>
2019-12-09 03:50:04 +0300

    Created cron job for publishing advertisements(in 1 minute for developement purposes)

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,3 +1,6 @@
+set :output, {:error => '/home/artgolovin/Documents/prepare_for_job/my-kufar/log/cron_error.log', :standard => '/home/artgolovin/Documents/prepare_for_job/my-kufar/log/cron_status.log'}
+env :PATH, ENV['PATH']
+
 every 1.minute do
-  runner "Advertisement.where(status: :approved).update(status: :published)"
+  runner "Advertisement.where(status: :approved).update(status: :published)", environment: :production
 end
